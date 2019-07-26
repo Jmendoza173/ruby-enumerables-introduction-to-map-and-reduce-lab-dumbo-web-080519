abf4bf3eb456b55e7dd9e2a73981c5bcc35f46b9
Joel Mendoza <joelmendza173@gmail.com>
2019-07-26 06:41:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,5 +21,5 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 #def reduce_to_total(source_array, starting_point)
-  source_array.reduce(starting_point, :+)
-end
+  #source_array.reduce(starting_point, :+)
+#end
