536450a1a5823e901f51963b2536e006d050cf5c
Joel Mendoza <joelmendza173@gmail.com>
2019-07-26 06:40:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,6 +20,6 @@ def reduce_to_total(source_array, starting_point=0)
   source_array.inject(starting_point) { |sum, n| sum + n }
 end
 
-def reduce_to_total(source_array, starting_point)
+#def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point, :+)
 end
